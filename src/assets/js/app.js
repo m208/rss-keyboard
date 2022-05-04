dbadb58ea99742bc6d9731fdcdee58823c75ff59
m208 <avcvet@yandex.ru>
2022-05-05 00:45:57 +0300

    feat: add local saving language settings

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,25 +6,27 @@ import Title from './Title';
 
 export default class App {
   constructor() {
+    const lang = this.storage('lang');
     const title = new Title('RSS Virtual Keyboard');
     this.output = new TextOutput(this);
-    const keyboard = new Keyboard(this, 'ru');
+    const keyboard = new Keyboard(this, lang);
     const desc = new Description();
     this.audio = new Audio('/dist/sound/clc1.mp3');
 
     document.body.addEventListener('keydown', (e) => {
-      const navKeys = ['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
       e.preventDefault();
-      if (navKeys.includes(e.code)) {
-        // console.log(e);
-      }
-
       keyboard.keyDown(e.code);
     });
     document.body.addEventListener('keyup', (e) => {
       e.preventDefault();
       keyboard.keyUp(e.code);
     });
+  }
+
+  storage(key, data = null) {
+    if (!data) return JSON.parse(localStorage.getItem(key));
+    localStorage.setItem(key, JSON.stringify(data));
+    return this; // airbnb wants it
   }
 
   sendKey(value) {
@@ -36,7 +38,7 @@ export default class App {
   }
 
   switchLang(lang) {
-    this.lang = lang; // temp
+    this.storage('lang', lang);
   }
 
   playSound() {
